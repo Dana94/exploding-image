c94ec2ca999ea79b034b99751db1d5344150d45d
Dana Ottaviani <dana.ottaviani@gmail.com>
2019-07-21 10:32:06 -0400

    Apply rows/columns to main component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,30 @@
 <template>
-  <main>
+  <!-- <main>
     <div class="title">
       <h1>Title</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       <button>Explore</button>
     </div>
     <div>
-      <!-- <exploder></exploder> -->
     </div>
     <section v-for="i in 5" :key="i.id" :class="{dark: i % 2 == 0}">
     </section>
-  </main>
+  </main> -->
+
+  <b-container>
+    <b-row>
+      <b-col cols="6" class="text-left">
+        <h1>Title</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        <button type="button" class="btn btn-warning">Explore</button>
+      </b-col>
+      <b-col cols="6">Column</b-col>
+    </b-row>
+    <b-row class="section" v-for="i in 5" :key="i.id" :class="{dark: i % 2 == 0}">
+       <b-col cols="12">Column</b-col>
+    </b-row>
+  </b-container>
+
 </template>
 
 <script>
@@ -23,41 +37,37 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-main {
-  width: 75%;
-  margin: 0 auto;
-  .title {
-    float: left;
-    display: flex;
-    flex-direction: column;
-    * {
-      display: inline-flex;
-    }
-    button {
-      background-color: #ffcc00;
-      border-radius: 10px;
-      border-style: none;
-      width: 8rem;
-      height: 3rem;
-      display: inline-block;
-    }
-  }
+// main {
+//   width: 75%;
+//   margin: 0 auto;
+//   .title {
+//     float: left;
+//     display: flex;
+//     flex-direction: column;
+//     * {
+//       display: inline-flex;
+//     }
+//   }
 
-  & > div {
-    width: 50%;
-  }
-}
+// section {
+//   height: 250px;
+//   margin: 0 auto;
+//   &.dark {
+//     background-color: #ffffff;
+//     border-radius: 10px;
+//     opacity: 0.25;
+//   }
+//   @media screen and (max-width: 425px) {
+//     // margin-left: 17%;
+//   }
+// }
 
-section {
+.section {
   height: 250px;
-  margin: 0 auto;
-  &.dark {
+  &.dark > div {
     background-color: #ffffff;
     border-radius: 10px;
     opacity: 0.25;
-  }
-  @media screen and (max-width: 425px) {
-    // margin-left: 17%;
   }
 }
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,20 @@
 <template>
-  <nav>
+  <!-- <nav>
     <ul>
       <li>Logo</li>
       <li v-for="i in 5" :key="i.id">Link</li>
       <li class="expand">Expand</li>
     </ul>
 
-  </nav>
+  </nav> -->
+  <div>
+    <b-nav>
+      <b-nav-item active>Active</b-nav-item>
+      <b-nav-item>Link</b-nav-item>
+      <b-nav-item>Another Link</b-nav-item>
+      <b-nav-item disabled>Disabled</b-nav-item>
+    </b-nav>
+  </div>
 </template>
 
 <script>
@@ -16,18 +24,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  ul {
-    display: flex;
-    justify-content: flex-start;
-    list-style: none;
-    padding-left: 0;
-    li {
-      margin: 0 20px;
-      color: #f6f3f1;
-      &.expand {
-        position: absolute;
-        right: 0;
-      }
-    }
+  // ul {
+  //   display: flex;
+  //   justify-content: flex-start;
+  //   list-style: none;
+  //   padding-left: 0;
+  //   li {
+  //     margin: 0 20px;
+  //     color: #f6f3f1;
+  //     &.expand {
+  //       position: absolute;
+  //       right: 0;
+  //     }
+  //   }
+  // }
+  .nav-item a {
+    color: #f6f3f1;
   }
 </style>
